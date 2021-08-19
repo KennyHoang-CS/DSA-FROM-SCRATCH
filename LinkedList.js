1e6c3915b01526c6b693823668dbade08369f560
KennyHoang-CS <kennyhoangcs@gmail.com>
2021-08-19 09:21:40 -0700

    more linkedlist functions

--- LinkedList.js
+++ LinkedList.js
@@ -26,6 +26,46 @@ class LinkedList {
         ++this.size; 
     };
 
+    addAtPosition(newNode, pos) {
+
+        // check if its a valid position.
+        if (pos < 0 || pos > this.size) {
+            console.log(`Must be a valid position from 1-${this.size}`);
+            return; 
+        }
+        
+        // adding at the head? pos = 1. 
+        if (pos === 1) {
+            newNode.next = this.head;
+            this.head = newNode; 
+            ++this.size; 
+            return;
+        }
+
+        // adding at the tail? 
+        if (pos === this.size) {
+            this.tail.next = newNode;
+            this.tail = newNode;
+            ++this.size; 
+            return;
+        }
+
+        // set up the pointers at the position for new node placement. 
+        let currPtr = this.head.next;
+        let prev = this.head;  
+        let idx = 1;
+        while (idx < pos - 1) {
+            prev = currPtr;  
+            currPtr = currPtr.next;  
+            ++idx;
+        }
+
+        // pointers are now at position, now insert new node. 
+        prev.next = newNode; 
+        newNode.next = currPtr; 
+        ++this.size; 
+    }
+
     delete(target) {
 
         // does the list even exist?
@@ -33,10 +73,18 @@ class LinkedList {
             return; 
         }
 
+        // are we deleting the head? 
+        if (this.head.data === target && (this.size > 1)) {
+            this.head = this.head.next; 
+            --this.size; 
+            return; 
+        }
+
         // only one node in list? Check if its the target. 
         if (this.size === 1) {
             if (this.head.data === target) {
                 this.head = null;
+                this.tail = null; 
                 --this.size;
             } else {
                 return; 
@@ -57,6 +105,7 @@ class LinkedList {
             currPtr = currPtr.next; 
             prevPtr = prevPtr.next; 
         }
+        --this.size; 
     };
     
     traversal() {
@@ -67,19 +116,33 @@ class LinkedList {
         }
     };
 
+    // helpers 
+
     getSize() {
         return this.size; 
     };
+
+    getHead() {
+        return this.head; 
+    }
+
+    getTail() {
+        return this.tail; 
+    }
 }
 
 let li = new LinkedList();
 li.add(new Node(1));
+/*
 li.add(new Node(2));
 li.add(new Node(5));
 li.add(new Node(3));
 li.add(new Node(10));
-li.traversal();
-li.delete(10);
-li.add(new Node(99));
-console.log('---------')
-li.traversal();
+*/
+
+// li.delete(1);
+// li.delete(10);
+// li.delete(3);
+ console.log(li.traversal());
+// console.log(li.getHead().data);
+// console.log(li.getTail().data);
