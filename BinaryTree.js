f35a88fe21e0bc8d1275c6dd8243ec110d43463d
KennyHoang-CS <kennyhoangcs@gmail.com>
2021-08-20 22:02:34 -0700

    initial Binary Tree skeleton

---
+++ BinaryTree.js
@@ -0,0 +1,32 @@
+
+class Node {
+    constructor(data, left = null, right = null) {
+        this.data = data;
+        this.left = left;
+        this.right = right; 
+    }
+}
+
+class BinaryTree {
+    constructor(root = null) {
+        this.root = root; 
+    }
+
+    // add
+    add(data) {
+
+    }
+
+    // delete
+    delete(target) {
+
+    }
+    
+    // search
+    search(target) {
+        
+    }
+
+    // traversals 
+}
+
